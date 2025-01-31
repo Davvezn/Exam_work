b195897c4d09032d62a6f4820596748c3858404a
David L <117825871+Davvezn@users.noreply.github.com>
2025-01-31 09:34:23 +0100

    first prepping of Exam work.
    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+</style>
