a40aedacbaf5137fadcc4f4074ec0dd8a36d3853
David L <117825871+Davvezn@users.noreply.github.com>
2025-02-14 02:33:32 +0100

    Added Simple Calc Feature
    added a small demo for how the income calculator will work.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const Database = await mongoose.connect("<Database_Key>")
+const Database = await mongoose.connect("<API_KEY>")
 new Elysia()
 
   .post("/Register-user", async({body, set}) => {
@@ -70,9 +70,44 @@ new Elysia()
     const all_users = await User.find()
     return(all_users)
   })
-  .get("/:user/all-data", async() => {
-    
+  .get("/:user/all-data", async() => { 
+    try {
+      // check for active tokens (dunno how I'll do that but manually for now.)
+      const user_data = await User.findOne()
+    } catch (error) {
+      
+    }
   })
+  .post("/income-calc", async({ body, set }) => {
+    const {user_income} = body as {user_income: number};
+    if (!user_income || user_income <= 0) {
+      set.status = 400;
+      return {message: "Please provide proper income amount (not to be rude if economy is shite)."};
+    }
+    // future update (spring 4 MAYBE) might add added detail such as Limit for certain costs aka food
+    // and then have a people calculator that adds maximum amount of money to be spent on ... and multiply with x amount of people
+    const inc_tax = user_income * 0.33; // could make a value for money left AFTER tax. 
+    const housing_costs = user_income * 0.25;
+    const food_costs = user_income * 0.10;
+    const fun_costs = user_income * 0.05;
+    const other_expenses = user_income * 0.07;
+    
+    const total_costs = inc_tax + housing_costs + food_costs + fun_costs + other_expenses;
+    const remaining_money = user_income - total_costs; 
+
+
+    return {
+      original_income: user_income,
+      tax: inc_tax.toLocaleString("sv-SE"),
+      housing: housing_costs.toLocaleString("sv-SE"),
+      food: food_costs.toLocaleString("sv-SE"),
+      fun: fun_costs.toLocaleString("sv-SE"),
+      other: other_expenses.toLocaleString("sv-SE"),
+      total_costs: total_costs.toLocaleString("sv-SE"),
+      remaining_money: remaining_money.toLocaleString("sv-SE"),
+    };
+  })
+
 
   .listen(3000)
   console.log("server is running on http://localhost:3000");
